a93260a9ce25424ca249b79c93ed3de3f0c6755f
PVHuy95 <phuvinhhuy1995@gmail.com>
2025-12-28 20:15:27 +0700

    update

--- database/migrations/2025_04_22_112635_create_booking_rooms_table.php
+++ database/migrations/2025_04_22_112635_create_booking_rooms_table.php
@@ -23,6 +23,16 @@ return new class extends Migration
             $table->integer('member'); // Số người lớn
             $table->datetime('checkin_date'); // Ngày check-in
             $table->datetime('checkout_date'); // Ngày check-out
+
+            $table->dateTime('actual_check_in')->nullable(); // Thời gian check-in thực tế
+            $table->dateTime('actual_check_out')->nullable(); // Thời gian check-out thực tế
+            $table->decimal('price', 15, 2)->nullable(); // Giá phòng
+            $table->decimal('total_price', 15, 2)->nullable(); // Tổng tiền
+            $table->unsignedBigInteger('room_id')->nullable(); // ID phòng cụ thể (nếu có)
+            $table->decimal('deposit_paid', 15, 2)->default(0); // Tiền cọc đã trả
+            $table->string('status')->default('pending'); // Trạng thái đặt phòng (confirmed, cancelled...)
+            $table->string('check_status')->nullable(); // Trạng thái check-in (checked in, checked out)
+
             $table->timestamps(); // Thời gian tạo và cập nhật
 
             // Khóa ngoại liên kết với bảng users
--- database/migrations/2025_05_28_140817_create_reviews_table.php
+++ database/migrations/2025_05_28_140817_create_reviews_table.php
@@ -9,14 +9,17 @@ class CreateReviewsTable extends Migration
     public function up()
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->id(); // Cột id (khóa chính)
-            $table->bigInteger('user_id')->unsigned()->nullable(); // user_id có thể null
-            $table->string('email', 191)->collation('utf8mb4_unicode_ci'); // email tối đa 191 ký tự
-            $table->integer('rating'); // Đánh giá
-            $table->text('comment')->collation('utf8mb4_general_ci')->nullable(); // Bình luận, có thể null
-            $table->text('admin_reply')->collation('utf8mb4_general_ci')->nullable(); // Phản hồi admin, có thể null
-            $table->dateTime('created_at'); // Thời gian tạo
-            $table->timestamp('updated_at')->nullable(); // Thời gian cập nhật, có thể null
+            $table->id(); 
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            
+            $table->string('email', 191); 
+            
+            $table->integer('rating');
+            
+            $table->text('comment')->nullable(); 
+            $table->text('admin_reply')->nullable(); 
+            
+            $table->timestamps(); 
         });
     }
 
